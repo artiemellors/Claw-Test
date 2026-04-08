Alicezz/openclaw

feat: [[SPLIT]] directive for multi-bubble WhatsApp replies + @mention context enrichment
Add [[SPLIT]] inline directive that breaks assistant replies into separate
WhatsApp message bubbles with human-like delivery — works in both streaming
(block chunker) and final delivery paths, including media+caption sends.

Also enrich inbound context with @mention JID resolution so agents can
identify who was mentioned and whether the bot itself was tagged.

Source changes:
- src/utils/split-tag.ts: core parser (code-fence-aware splitting)
- src/utils/split-tag.test.ts: 14 unit tests
- src/utils/directive-tags.ts: strip [[SPLIT]] from display text
- src/agents/pi-embedded-subscribe.ts: split in streaming block chunker
- src/infra/outbound/deliver.ts: split in sendTextChunks + media caption path
- src/auto-reply/templating.ts: MentionedJids, SelfJid, SelfE164, MentionedContacts fields
- src/auto-reply/reply/inbound-meta.ts: emit self_jid, self_e164, mentioned_jids/contacts
- src/web/auto-reply/monitor/process-message.ts: buildMentionedContacts() resolver

Co-Authored-By: Claude Opus 4.6 <noreply@anthropic.com>

src/web/auto-reply/monitor/process-message.ts
@@ -49,6 +49,56 @@ export type GroupHistoryEntry = {
   senderJid?: string;
 };
 
+/**
+ * Build a human-readable mention contact map so the model knows who was @mentioned.
+ * Resolves JIDs to names via the group roster, and marks the bot's own JID as "you (self)".
+ */
+function buildMentionedContacts(params: {
+  mentionedJids?: string[];
+  roster?: Map<string, string>;
+  selfJid?: string | null;
+  selfE164?: string | null;
+}): string | undefined {
+  const { mentionedJids, roster, selfJid, selfE164 } = params;
+  if (!mentionedJids?.length) {
+    return undefined;
+  }
+
+  // Normalize selfJid by stripping device suffix (e.g. :0, :14)
+  const normalizedSelf = selfJid ? selfJid.replace(/:\d+/, "") : null;
+
+  const entries: string[] = [];
+  for (const jid of mentionedJids) {
+    // Check if this is the bot's own JID/LID
+    if (normalizedSelf && jid === normalizedSelf) {
+      entries.push(`${jid} = you (self)`);
+      continue;
+    }
+    // Also check E164 match for self (handles standard JID format)
+    if (selfE164) {
+      const jidE164 = jidToE164(jid);
+      if (jidE164 && normalizeE164(jidE164) === normalizeE164(selfE164)) {
+        entries.push(`${jid} = you (self)`);
+        continue;
+      }
+    }
+    // Try to resolve via E164 + group roster
+    const e164 = jidToE164(jid);
+    if (e164 && roster) {
+      const normalized = normalizeE164(e164);
+      const name = normalized ? roster.get(normalized) : undefined;
+      if (name) {
+        entries.push(`${jid} = ${name}`);
+        continue;
+      }
+    }
+    // LID or unresolvable — include raw JID
+    entries.push(jid);
+  }
+
+  return entries.join(", ");
+}
+
 async function resolveWhatsAppCommandAuthorized(params: {
   cfg: ReturnType<typeof loadConfig>;
   msg: WebInboundMsg;
@@ -326,6 +376,15 @@ export async function processMessage(params: {
     SenderE164: params.msg.senderE164,
     CommandAuthorized: commandAuthorized,
     WasMentioned: params.msg.wasMentioned,
+    MentionedJids: params.msg.mentionedJids,
+    SelfJid: params.msg.selfJid ?? undefined,
+    SelfE164: params.msg.selfE164 ?? undefined,
+    MentionedContacts: buildMentionedContacts({
+      mentionedJids: params.msg.mentionedJids,
+      roster: params.groupMemberNames.get(params.groupHistoryKey),
+      selfJid: params.msg.selfJid,
+      selfE164: params.msg.selfE164,
+    }),
     ...(params.msg.location ? toLocationContext(params.msg.location) : {}),
     Provider: "whatsapp",
     Surface: "whatsapp",

artiemellors/Claw-Test

extensions/whatsapp/src/auto-reply/monitor/process-message.ts
@@ -42,6 +42,56 @@ import {
   type resolveAgentRoute,
 } from "./runtime-api.js";
 
+/**
+ * Build a human-readable mention contact map so the model knows who was @mentioned.
+ * Resolves JIDs to names via the group roster, and marks the bot's own JID as "you (self)".
+ */
+function buildMentionedContacts(params: {
+  mentionedJids?: string[];
+  roster?: Map<string, string>;
+  selfJid?: string | null;
+  selfE164?: string | null;
+}): string | undefined {
+  const { mentionedJids, roster, selfJid, selfE164 } = params;
+  if (!mentionedJids?.length) {
+    return undefined;
+  }
+
+  // Normalize selfJid by stripping device suffix (e.g. :0, :14)
+  const normalizedSelf = selfJid ? selfJid.replace(/:\d+/, "") : null;
+
+  const entries: string[] = [];
+  for (const jid of mentionedJids) {
+    // Check if this is the bot's own JID/LID
+    if (normalizedSelf && jid === normalizedSelf) {
+      entries.push(`${jid} = you (self)`);
+      continue;
+    }
+    // Also check E164 match for self (handles standard JID format)
+    if (selfE164) {
+      const jidE164 = jidToE164(jid);
+      if (jidE164 && normalizeE164(jidE164) === normalizeE164(selfE164)) {
+        entries.push(`${jid} = you (self)`);
+        continue;
+      }
+    }
+    // Try to resolve via E164 + group roster
+    const e164 = jidToE164(jid);
+    if (e164 && roster) {
+      const normalized = normalizeE164(e164);
+      const name = normalized ? roster.get(normalized) : undefined;
+      if (name) {
+        entries.push(`${jid} = ${name}`);
+        continue;
+      }
+    }
+    // LID or unresolvable — include raw JID
+    entries.push(jid);
+  }
+
+  return entries.join(", ");
+}
+
 async function resolveWhatsAppCommandAuthorized(params: {
   cfg: ReturnType<LoadConfigFn>;
   msg: WebInboundMsg;
@@ -303,6 +353,16 @@ export async function processMessage(params: {
       e164: sender.e164 ?? undefined,
     },
     visibleReplyTo: visibleReplyTo ?? undefined,
+    // shoar local: mention enrichment so the model knows exactly who was @mentioned in groups
+    mentionedJids: params.msg.mentionedJids,
+    selfJid: params.msg.selfJid ?? undefined,
+    selfE164: params.msg.selfE164 ?? undefined,
+    mentionedContacts: buildMentionedContacts({
+      mentionedJids: params.msg.mentionedJids,
+      roster: params.groupMemberNames.get(params.groupHistoryKey),
+      selfJid: params.msg.selfJid,
+      selfE164: params.msg.selfE164,
+    }),
   });
 
   const pinnedMainDmRecipient = resolvePinnedMainDmRecipient({