kbarbel640-del/openclaw

feat(workspace): add tiered bootstrap loading with configurable bootstrapTier
Introduces a three-tier bootstrap file loading system:
- 'minimal': AGENTS.md + TOOLS.md only (default for subagent/cron sessions)
- 'standard': All recognized bootstrap files (default for main sessions)
- 'full': Standard set plus extra bootstrap file patterns

The tier can be configured via agents.defaults.bootstrapTier, allowing
users to override the default session-type heuristic. For example,
subagent sessions can be promoted to 'standard' to receive SOUL.md,
IDENTITY.md, and other persona files.

Changes:
- Add BootstrapTier type and resolveBootstrapTier() to workspace.ts
- Extend filterBootstrapFilesForSession() with optional tierOverride
- Add bootstrapTier to config schema, types, labels, and help text
- Wire tier resolution through bootstrap-files.ts
- Add comprehensive test suite (11 tests)

src/config/schema.help.ts
@@ -160,6 +160,8 @@ export const FIELD_HELP: Record<string, string> = {
     "Optional per-provider overrides for billing backoff (hours).",
   "auth.cooldowns.billingMaxHours": "Cap (hours) for billing backoff (default: 24).",
   "auth.cooldowns.failureWindowHours": "Failure window (hours) for backoff counters (default: 24).",
+  "agents.defaults.bootstrapTier":
+    'Bootstrap loading tier: "minimal" (AGENTS.md + TOOLS.md), "standard" (all files, default), or "full" (standard + extra patterns).',
   "agents.defaults.bootstrapMaxChars":
     "Max characters of each workspace bootstrap file injected into the system prompt before truncation (default: 20000).",
   "agents.defaults.bootstrapTotalMaxChars":

artiemellors/Claw-Test

src/config/schema.help.ts
@@ -828,6 +828,8 @@ export const FIELD_HELP: Record<string, string> = {
     "Default workspace path exposed to agent runtime tools for filesystem context and repo-aware behavior. Set this explicitly when running from wrappers so path resolution stays deterministic.",
   "agents.defaults.contextInjection":
     'Controls when workspace bootstrap files are injected into the system prompt: "always" (default) or "continuation-skip" for safe continuation turns after a completed assistant response.',
+  "agents.defaults.bootstrapTier":
+    'Bootstrap loading tier: "minimal" (AGENTS.md + TOOLS.md), "standard" (recognized files, default), or "full" (standard + extra patterns).',
   "agents.defaults.bootstrapMaxChars":
     "Max characters of each workspace bootstrap file injected into the system prompt before truncation (default: 20000).",
   "agents.defaults.bootstrapTotalMaxChars":