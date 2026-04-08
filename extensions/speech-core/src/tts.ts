openclaw/openclaw

fix(whatsapp): repair web auto-TTS delivery

src/tts/tts.ts
@@ -26,6 +26,7 @@ import { logVerbose } from "../globals.js";
 import { resolvePreferredOpenClawTmpDir } from "../infra/tmp-openclaw-dir.js";
 import { stripMarkdown } from "../line/markdown-to-line.js";
 import { isVoiceCompatibleAudio } from "../media/audio.js";
+import { runFfmpeg } from "../media/ffmpeg-exec.js";
 import { CONFIG_DIR, resolveUserPath } from "../utils.js";
 import {
   DEFAULT_OPENAI_BASE_URL,
@@ -503,6 +504,46 @@ export function setLastTtsAttempt(entry: TtsStatusEntry | undefined): void {
 /** Channels that require opus audio and support voice-bubble playback */
 const VOICE_BUBBLE_CHANNELS = new Set(["telegram", "feishu", "whatsapp"]);
 
+async function maybeNormalizeVoiceBubbleAudio(params: {
+  audioPath: string;
+  channelId: ChannelId | null;
+}): Promise<string> {
+  if (params.channelId === null || !VOICE_BUBBLE_CHANNELS.has(params.channelId)) {
+    return params.audioPath;
+  }
+  if (path.extname(params.audioPath).toLowerCase() !== ".webm") {
+    return params.audioPath;
+  }
+
+  const outputPath = path.join(
+    path.dirname(params.audioPath),
+    `${path.basename(params.audioPath, ".webm")}.ogg`,
+  );
+
+  try {
+    await runFfmpeg([
+      "-y",
+      "-i",
+      params.audioPath,
+      "-vn",
+      "-sn",
+      "-dn",
+      "-ar",
+      "48000",
+      "-ac",
+      "1",
+      "-c:a",
+      "libopus",
+      "-b:a",
+      "64k",
+      outputPath,
+    ]);
+    return outputPath;
+  } catch {
+    return params.audioPath;
+  }
+}
+
 function resolveOutputFormat(channelId?: string | null) {
   if (channelId && VOICE_BUBBLE_CHANNELS.has(channelId)) {
     return TELEGRAM_OUTPUT;
@@ -645,15 +686,20 @@ export async function textToSpeech(params: {
           }
         }
 
+        const normalizedAudioPath = await maybeNormalizeVoiceBubbleAudio({
+          audioPath: edgeResult.audioPath,
+          channelId,
+        });
         scheduleCleanup(tempDir);
-        const voiceCompatible = isVoiceCompatibleAudio({ fileName: edgeResult.audioPath });
+        const voiceCompatible = isVoiceCompatibleAudio({ fileName: normalizedAudioPath });
 
         return {
           success: true,
-          audioPath: edgeResult.audioPath,
+          audioPath: normalizedAudioPath,
           latencyMs: Date.now() - providerStart,
           provider,
-          outputFormat: edgeResult.outputFormat,
+          outputFormat:
+            normalizedAudioPath === edgeResult.audioPath ? edgeResult.outputFormat : "ogg-opus",
           voiceCompatible,
         };
       }
@@ -976,4 +1022,5 @@ export const _test = {
   summarizeText,
   resolveOutputFormat,
   resolveEdgeOutputFormat,
+  maybeNormalizeVoiceBubbleAudio,
 };

artiemellors/Claw-Test

extensions/speech-core/src/tts.ts
@@ -20,6 +20,7 @@ import type {
 } from "openclaw/plugin-sdk/config-runtime";
 import { formatErrorMessage } from "openclaw/plugin-sdk/error-runtime";
 import { redactSensitiveText } from "openclaw/plugin-sdk/logging-core";
+import { runFfmpeg } from "openclaw/plugin-sdk/media-runtime";
 import { resolveSendableOutboundReplyParts } from "openclaw/plugin-sdk/reply-payload";
 import type { ReplyPayload } from "openclaw/plugin-sdk/reply-runtime";
 import { isVerbose, logVerbose } from "openclaw/plugin-sdk/runtime-env";
@@ -595,6 +596,46 @@ export function setLastTtsAttempt(entry: TtsStatusEntry | undefined): void {
 
 const OPUS_CHANNELS = new Set(["telegram", "feishu", "whatsapp", "matrix"]);
 
+async function maybeNormalizeVoiceBubbleAudio(params: {
+  audioPath: string;
+  channelId: ChannelId | null;
+}): Promise<string> {
+  if (params.channelId === null || !OPUS_CHANNELS.has(params.channelId)) {
+    return params.audioPath;
+  }
+  if (path.extname(params.audioPath).toLowerCase() !== ".webm") {
+    return params.audioPath;
+  }
+
+  const outputPath = path.join(
+    path.dirname(params.audioPath),
+    `${path.basename(params.audioPath, ".webm")}.ogg`,
+  );
+
+  try {
+    await runFfmpeg([
+      "-y",
+      "-i",
+      params.audioPath,
+      "-vn",
+      "-sn",
+      "-dn",
+      "-ar",
+      "48000",
+      "-ac",
+      "1",
+      "-c:a",
+      "libopus",
+      "-b:a",
+      "64k",
+      outputPath,
+    ]);
+    return outputPath;
+  } catch {
+    return params.audioPath;
+  }
+}
+
 function resolveChannelId(channel: string | undefined): ChannelId | null {
   return channel ? normalizeChannelId(channel) : null;
 }
@@ -772,18 +813,23 @@ export async function textToSpeech(params: {
   const tempDir = mkdtempSync(path.join(tempRoot, "tts-"));
   const audioPath = path.join(tempDir, `voice-${Date.now()}${synthesis.fileExtension}`);
   writeFileSync(audioPath, synthesis.audioBuffer);
+  const normalizedAudioPath = await maybeNormalizeVoiceBubbleAudio({
+    audioPath,
+    channelId: resolveChannelId(params.channel),
+  });
   scheduleCleanup(tempDir);
 
   return {
     success: true,
-    audioPath,
+    audioPath: normalizedAudioPath,
     latencyMs: synthesis.latencyMs,
     provider: synthesis.provider,
     fallbackFrom: synthesis.fallbackFrom,
     attemptedProviders: synthesis.attemptedProviders,
     attempts: synthesis.attempts,
-    outputFormat: synthesis.outputFormat,
-    voiceCompatible: synthesis.voiceCompatible,
+    outputFormat: normalizedAudioPath === audioPath ? synthesis.outputFormat : "ogg-opus",
+    voiceCompatible:
+      normalizedAudioPath === audioPath ? synthesis.voiceCompatible : true,
   };
 }
 
@@ -1187,6 +1233,7 @@ export async function maybeApplyTtsToPayload(params: {
 }
 
 export const _test = {
+  maybeNormalizeVoiceBubbleAudio,
   parseTtsDirectives,
   resolveModelOverridePolicy,
   summarizeText,