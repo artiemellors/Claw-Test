openclaw/openclaw

sandbox: add workspaceMountPropagation config for Docker workspace mount
Add `sandbox.docker.workspaceMountPropagation` option ("private" | "rslave" |
"rshared") that controls the bind propagation mode of the workspace mount
passed to `docker create -v`.

Default is "private" (existing behavior). Setting "rslave" causes host-level
bind mounts inside the workspace directory to propagate into the container,
enabling use cases where a subdirectory of the workspace is itself a bind
mount on the host (e.g. `mount --bind /other/path workspace/subdir`).

The host mount point must have shared propagation (`mount --make-shared`)
for rslave to take effect.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

src/agents/sandbox/workspace-mounts.test.ts
@@ -3,7 +3,7 @@ import { appendWorkspaceMountArgs } from "./workspace-mounts.js";
 
 describe("appendWorkspaceMountArgs", () => {
   it.each([
-    { access: "rw" as const, expected: "/tmp/workspace:/workspace" },
+    { access: "rw" as const, expected: "/tmp/workspace:/workspace:rw" },
     { access: "ro" as const, expected: "/tmp/workspace:/workspace:ro" },
     { access: "none" as const, expected: "/tmp/workspace:/workspace:ro" },
   ])("sets main mount permissions for workspaceAccess=$access", ({ access, expected }) => {
@@ -44,6 +44,42 @@ describe("appendWorkspaceMountArgs", () => {
     });
 
     const mounts = args.filter((arg) => arg.startsWith("/tmp/"));
-    expect(mounts).toEqual(["/tmp/workspace:/workspace"]);
+    expect(mounts).toEqual(["/tmp/workspace:/workspace:rw"]);
+  });
+
+  it.each([
+    { propagation: "private" as const, expected: "/tmp/workspace:/workspace:rw" },
+    { propagation: "rslave" as const, expected: "/tmp/workspace:/workspace:rw,rslave" },
+    { propagation: "rshared" as const, expected: "/tmp/workspace:/workspace:rw,rshared" },
+  ])(
+    "appends propagation mode to workspace mount for workspaceMountPropagation=$propagation",
+    ({ propagation, expected }) => {
+      const args: string[] = [];
+      appendWorkspaceMountArgs({
+        args,
+        workspaceDir: "/tmp/workspace",
+        agentWorkspaceDir: "/tmp/agent-workspace",
+        workdir: "/workspace",
+        workspaceAccess: "rw",
+        workspaceMountPropagation: propagation,
+      });
+
+      expect(args).toContain(expected);
+    },
+  );
+
+  it("does not append propagation to agent workspace mount", () => {
+    const args: string[] = [];
+    appendWorkspaceMountArgs({
+      args,
+      workspaceDir: "/tmp/workspace",
+      agentWorkspaceDir: "/tmp/agent-workspace",
+      workdir: "/workspace",
+      workspaceAccess: "rw",
+      workspaceMountPropagation: "rslave",
+    });
+
+    expect(args).toContain("/tmp/workspace:/workspace:rw,rslave");
+    expect(args).toContain("/tmp/agent-workspace:/agent");
   });
 });

artiemellors/Claw-Test

src/agents/sandbox/workspace-mounts.test.ts
@@ -3,7 +3,7 @@ import { appendWorkspaceMountArgs } from "./workspace-mounts.js";
 
 describe("appendWorkspaceMountArgs", () => {
   it.each([
-    { access: "rw" as const, expected: "/tmp/workspace:/workspace:z" },
+    { access: "rw" as const, expected: "/tmp/workspace:/workspace:rw,z" },
     { access: "ro" as const, expected: "/tmp/workspace:/workspace:ro,z" },
     { access: "none" as const, expected: "/tmp/workspace:/workspace:ro,z" },
   ])("sets main mount permissions for workspaceAccess=$access", ({ access, expected }) => {
@@ -44,7 +44,43 @@ describe("appendWorkspaceMountArgs", () => {
     });
 
     const mounts = args.filter((arg) => arg.startsWith("/tmp/"));
-    expect(mounts).toEqual(["/tmp/workspace:/workspace:z"]);
+    expect(mounts).toEqual(["/tmp/workspace:/workspace:rw,z"]);
+  });
+
+  it.each([
+    { propagation: "private" as const, expected: "/tmp/workspace:/workspace:rw,z" },
+    { propagation: "rslave" as const, expected: "/tmp/workspace:/workspace:rw,rslave,z" },
+    { propagation: "rshared" as const, expected: "/tmp/workspace:/workspace:rw,rshared,z" },
+  ])(
+    "appends propagation mode to workspace mount for workspaceMountPropagation=$propagation",
+    ({ propagation, expected }) => {
+      const args: string[] = [];
+      appendWorkspaceMountArgs({
+        args,
+        workspaceDir: "/tmp/workspace",
+        agentWorkspaceDir: "/tmp/agent-workspace",
+        workdir: "/workspace",
+        workspaceAccess: "rw",
+        workspaceMountPropagation: propagation,
+      });
+
+      expect(args).toContain(expected);
+    },
+  );
+
+  it("does not append propagation to agent workspace mount", () => {
+    const args: string[] = [];
+    appendWorkspaceMountArgs({
+      args,
+      workspaceDir: "/tmp/workspace",
+      agentWorkspaceDir: "/tmp/agent-workspace",
+      workdir: "/workspace",
+      workspaceAccess: "rw",
+      workspaceMountPropagation: "rslave",
+    });
+
+    expect(args).toContain("/tmp/workspace:/workspace:rw,rslave,z");
+    expect(args).toContain("/tmp/agent-workspace:/agent:z");
   });
 
   it("marks split agent workspace mounts shared for SELinux", () => {